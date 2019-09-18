13662389c2f341b4b35af57eb287e2bda069b0b0
NexRox <oscar-alejandro1@live.com>
2019-09-18 18:01:47 -0500

    Python

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+num1 = input("Introduce el primer numero\n")
+num2 = input("Introduce el segundo numero\n")
+
+num1n= int(num1)
+num2n= int(num2)
+
+suma = num1n+num2n
+resta = num1n-num2n
+multiplicacion = num1n*num2n
+division = num1n/num2n
+
+print("El resultado de la suma es de: ",suma)
+print("El resultado de la resta es de: ",resta)
+print("El resultado de la multiplicacion es de: ",multiplicacion)
+print("El resultado de la division es de: ",division)
+
